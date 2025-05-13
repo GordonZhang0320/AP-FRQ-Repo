6a738bfd70fceaf7cededbd34d76b6baee769de5
GordonZhang0320 <gordonz14@nycstudents.net>
2025-05-13 11:44:54 -0400

    Update Part 1.java

--- Q2/Part 1.java
+++ Q2/Part 1.java
@@ -1,1 +1,31 @@
+public class SignedText{
+  private String fName;
+  private String lName;
 
+  public SignedText(String f, String l){
+    fName=f;
+    lName=l;
+  }
+
+  public String getSignature(){
+    if (fName.equals("")){
+      return lName;
+    }
+
+    return fName.substring(0,1)+lName;
+  }
+
+  public String addSignature(String str){
+    String signature=getSignature();
+    if (str.indexOf(signature)==0){
+      return str.substing(signature.length())+signature;
+    } else if (str.indexOf(signature)==-1)
+      return str+siganture;
+    }
+    return str;
+  }
+
+  
+
+
+}
