29bff754465de2237a4cd52460086a4885723682
GordonZhang0320 <gordonz14@nycstudents.net>
2025-05-15 11:55:43 -0400

    Update Part 1.java

--- Q3/Part 1.java
+++ Q3/Part 1.java
@@ -1,1 +1,6 @@
-
+ public Round(String[] names){
+        for (int i=0; i<names.length; i++){
+            Competitor c=new Competitor(names[i],i+1);
+            competitorList.add(c);
+        }
+}
