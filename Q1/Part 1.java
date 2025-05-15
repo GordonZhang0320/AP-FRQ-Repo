569fcce77d4f325d85032077d76388297c8577ec
GordonZhang0320 <gordonz14@nycstudents.net>
2025-05-15 15:25:03 +0000

    Update Part 1.java

--- Q1/Part 1.java
+++ Q1/Part 1.java
@@ -10,13 +10,3 @@
         return dogs;
     }
 
-    public int dogWalkShift(int startHour, int endHour){
-        int result=0;
-        for (int i=startHour; i<=endHour; i++){
-            if (walkDog(i)==maxDogs || (i>=9 && i>=17)){
-                result+=3;
-            }
-            result+=walkDogs(i)*5;
-        }
-        return result;
-  }
