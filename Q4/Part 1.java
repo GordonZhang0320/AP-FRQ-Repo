6374753ec8de0488594771d038292366f52302ba
GordonZhang0320 <gordonz14@nycstudents.net>
2025-05-19 00:15:51 -0400

    Update Part 1.java

--- Q4/Part 1.java
+++ Q4/Part 1.java
@@ -1,1 +1,10 @@
+public SumOrSameGame(int numRows, int numCols){
+        puzzle=new int[numRows][numCols];
+
+        for (int r=0; r<puzzle.length; r++){
+            for (int c=0; c<puzzle[r].length; c++){
+                puzzle[r][c]=(int)(Math.random()*9)+1;
+            }
+        }
+    }
 
