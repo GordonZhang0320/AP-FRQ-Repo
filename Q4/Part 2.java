0dea054a97bf4d5099d1922b59499e0cbb787ca6
GordonZhang0320 <gordonz14@nycstudents.net>
2025-05-19 00:16:23 -0400

    Create Part 2.java

---
+++ Q4/Part 2.java
@@ -0,0 +1,17 @@
+public boolean clearPair(int row, int col){
+        for (int r=row; r<puzzle.length; r++){
+            for (int c=0; c<puzzle[r].length; c++){
+                if (c!=col && r!=row){
+                    if (puzzle[row][col]==puzzle[r][c]){
+                        puzzle[row][col]=0;
+                        puzzle[r][c]=0;
+                    } 
+                    if (puzzle[row][col]+puzzle[r][c]==10){
+                        puzzle[row][col]=0;
+                        puzzle[r][c]=0;
+                    }
+                }
+            }
+        }
+        return false;
+    }
